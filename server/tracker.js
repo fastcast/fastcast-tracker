376dccfae75c77534c0f44cdef7fe453028eb785
jakefb <jacobafb@gmail.com>
2016-06-18 21:24:39 +1200

    Add redirect to website homepage, remove config, disable http

--- server/tracker.js
+++ server/tracker.js
@@ -1,18 +1,16 @@
-var config = require('../config')
-
 var Tracker = require('bittorrent-tracker/server')
 
-var tracker = new Tracker({
-  ws: true, // enable websocket (webtorrent) tracker,
-  udp: false // TEMPORARILY DISABLE UDP - it's making server fail to start in production
-             // since udp6 support was added
-})
+// Create WebTorrent-only tracker. Disable UDP and HTTP.
+var tracker = new Tracker({ ws: true, udp: false, http: false })
 
-tracker.on('listening', function () {
-  var ports = {
-    http: tracker.http.address().port
-    // udp: tracker.udp.address().port
+// Redirect http://tracker.fastcast.nz to website homepage
+tracker.http.on('request', function (req, res, opts) {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(301, { 'Location': 'https://fastcast.nz' })
+    res.end()
   }
 })
 
-tracker.listen(config.ports.tracker, { http: '127.0.0.1', udp: config.host })
+tracker.on('listening', function () {})
+
+tracker.listen('9003', '127.0.0.1')
